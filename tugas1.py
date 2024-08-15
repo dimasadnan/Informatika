8745859e3c845ebb7721611cbb2d014ea0beb437
Dimas Adnan Prayogo <144643340+DimasAdnan@users.noreply.github.com>
2024-08-15 17:29:44 +0700

    Update tugas1.py

--- tugas1.py
+++ tugas1.py
@@ -38,3 +38,13 @@ if (pendidikan == "s1" or pendidikan == "d3") and umur >= 21 and jeniskelamin ==
 else:
     print("nda boleh")
 
+x = 19
+
+if x > 5:
+    print("x is more than 5")
+elif x == 5:
+    print("x is equal to 5")
+else:
+    print("x is less than 5")
+
+
